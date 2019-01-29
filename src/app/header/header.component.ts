617d86473db483d77908597999de5c7faaffd8ad
Gen Next <35140819+gennext35@users.noreply.github.com>
2019-01-29 19:10:37 +0530

    initial commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  isIn = false;   // store state
+  toggleState() { // click handler
+      let bool = this.isIn;
+      this.isIn = bool === false ? true : false; 
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
